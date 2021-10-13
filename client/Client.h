b28672d4d0b7095f6d7abdc61d86322886e89b67
noamronen <42314592+noamronen@users.noreply.github.com>
2021-10-13 09:41:17 +0300

    Create Client.h

---
+++ client/Client.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <vector>
+#include <thread>
+#include <pthread.h>
+using namespace std;
+#pragma warning(disable:4996)
+
+class Client
+{
+private:
+    int client_socket;
+    struct sockaddr_in server_address;
+    char client_name[1024];
+
+public:
+    Client();
+    void openChat();
+    void Send();
+    void recieve();
+    void closeClient();
+
+};
