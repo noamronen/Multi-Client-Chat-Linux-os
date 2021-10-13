acf9874b6f7982698beb6037fba83bc995dd9b33
noamronen <42314592+noamronen@users.noreply.github.com>
2021-10-13 09:17:15 +0300

    Create server_main.cpp

---
+++ server/server_main.cpp
@@ -0,0 +1,15 @@
+#include "server.h"
+
+#pragma comment (lib, "ws2_32.lib") //including the library to the LINKER settings.
+
+using namespace std;
+int main()
+{
+    server serv = server();
+    serv.openChat();
+    serv.closeServer();
+
+    return 0;
+}
+
+
